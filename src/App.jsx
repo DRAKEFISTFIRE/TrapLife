2a51f137e8c430ce32db3a32d26c591bd0e6ca1c
Alekcaritg <alekcaritg2005@gmail.com>
2025-05-23 14:28:30 +0200

    cambiar titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import HeroSection from './components/HeroSection/HeroSection';
+import NuestroServidor from './components/NuestroServidor/NuestroServidor';
 
 function App() {
   return (
---
+++ src/components/NuestroServidor/NuestroServidor.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import NavBar from '../NavBar/NavBar';
+import './NuestroServidor.css';
+
+export default function NuestroServidor() {
+    return (
+        NavBar
+    )
+}
